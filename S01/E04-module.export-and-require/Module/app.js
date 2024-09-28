af2511e4a676a68217183977459f571deb035ec8
Ishank Suri <ishanksuri01@gmail.com>
2024-09-28 23:10:14 +0530

    Started S01-E04-Module

---
+++ S01/E04-module.export-and-require/Module/app.js
@@ -0,0 +1,6 @@
+// const { x, calculateSum } = require("./4sum.js");
+import { x, calculateSum } from "./sum.js";
+
+// var x = "100";//Error
+calculateSum(4, 3); // No Error
+console.log(x); // No Error
---
+++ S01/E04-module.export-and-require/Module/sum.js
@@ -0,0 +1,20 @@
+console.log("Sum Module Executed");
+
+export var x = "hello sum";
+
+//wont work in ESM but will work in CJSff
+z = "Hello World";
+
+//new way to export when type is module
+// ESM or mjs
+export function calculateSum(a, b) {
+  const sum = a + b;
+  console.log(sum);
+}
+
+//create obj to
+// with explicit export
+//multiple items
+// module.exports = { x: x, calculateSum: calculateSum };
+//OR
+// module.exports = { x, calculateSum };
