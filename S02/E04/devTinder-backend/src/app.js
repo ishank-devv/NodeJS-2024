3d90a82f356f43830fefb247293677084c46aa0d
Ishank Suri <ishanksuri01@gmail.com>
2024-12-19 23:01:04 +0530

    Advance routing concepts & REGEX

--- S02/E04/devTinder-backend/src/app.js
+++ S02/E04/devTinder-backend/src/app.js
@@ -1,28 +1,45 @@
 const express = require("express");
 const app = express();
 
-// ORDER MATTERS- dont write app.use on top as it will be excuted first in place of GET POST & DELETE
-// app.use("/user", (req, res) => {
-//   res.send("HAHAHAHA");
-// });
-
-// Handling GET & POST call seperately
-// app.get- will match only GET requests
-// app.use- will match all HTTPS requests( including GET & POST )
-
-// This will only handle GET call to /user
-app.get("/user", (req, res) => {
-  res.send({ firstname: "Varun", lastname: "Mehta" });
+// Advanced routing concepts
+// 1. /ab?c - means b is optional over here due to ?, means "ac" will work as well
+// /ac & /abc both will work
+app.get("/ab?c", (req, res) => {
+  res.send({ firstname: "ab?c", lastname: "Mehta" });
 });
 
-// This will only handle POST call to /user
-app.post("/user", (req, res) => {
-  //assume saving data to db happened
-  res.send("Data successfully saved to the database!");
+//bc is optional so /ad will work
+app.get("/a(bc)?d", (req, res) => {
+  res.send({ firstname: "a(bc)?d", lastname: "Mehta" });
 });
 
-app.delete("/user", (req, res) => {
-  res.send("Deleted Successfully");
+// 2. /ab+c - means you can add as many b as you want to
+// /abc & /abbbbbbc will both work
+app.get("/ab+c", (req, res) => {
+  res.send({ firstname: "ab+c", lastname: "mehta" });
+});
+
+// /abcbcbcd will work
+app.get("/a(bc)+d", (req, res) => {
+  res.send({ firstname: "a(bc)+d", lastname: "mehta" });
+});
+
+// 3. /ab*cd - /ab(anythinginbetween)cd will work
+// /abcd & /abVARUNcd
+app.get("/ab*cd", (req, res) => {
+  res.send({ firstname: "abVARUNcd", lastname: "mehta" });
+});
+
+//4. if in the word, anywhere a letter is there it will work
+// /cab
+app.get(/a/, (req, res) => {
+  res.send({ firstname: "/a/", lastname: "mehta" });
+});
+
+//REGEX
+//5. /.*fly$/ - it wil work if anything starts with anything but surely ends with fly, dragonfly12 won't work
+app.get(/.*fly$/, (req, res) => {
+  res.send({ firstname: "endsWithFly", lastname: "mehta" });
 });
 
 app.listen(3000, () => {
---
+++ S02/E04/devTinder-backend/src/app_get_post_delete.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const app = express();
+
+// ORDER MATTERS- dont write app.use on top as it will be excuted first in place of GET POST & DELETE
+// app.use("/user", (req, res) => {
+//   res.send("HAHAHAHA");
+// });
+
+// Handling GET & POST call seperately
+// app.get- will match only GET requests
+// app.use- will match all HTTPS requests( including GET & POST )
+
+// This will only handle GET call to /user
+app.get("/user", (req, res) => {
+  res.send({ firstname: "Varun", lastname: "Mehta" });
+});
+
+// This will only handle POST call to /user
+app.post("/user", (req, res) => {
+  //assume saving data to db happened
+  res.send("Data successfully saved to the database!");
+});
+
+app.delete("/user", (req, res) => {
+  res.send("Deleted Successfully");
+});
+
+app.listen(3000, () => {
+  console.log("Server is successfully listening on port 3000...");
+});
