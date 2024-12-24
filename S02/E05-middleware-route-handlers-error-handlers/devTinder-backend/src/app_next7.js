a2624f6f907d882b82523bc4ce0f47035952f266
Ishank Suri <ishanksuri01@gmail.com>
2024-12-24 14:14:43 +0530

    next() with multiple route handlers function in app.use part2

---
+++ S02/E05-middleware-route-handlers-error-handlers/devTinder-backend/src/app_next7.js
@@ -0,0 +1,42 @@
+const express = require("express");
+const app = express();
+
+// ADDING MULTIPLE FUNCTIONS
+// All will give the same response - working is same
+// app.use("/route", rh1,rh2,rh3,rh4,rh5)
+// app.use("/route", [rh1,rh2,rh3,rh4,rh5])
+// app.use("/route", [rh1,rh2],rh3,rh4,rh5)
+app.use(
+  "/user",
+  [
+    (req, res, next) => {
+      //Route Handler 1
+      console.log("Handling the router user 1");
+      next();
+    },
+    (req, res, next) => {
+      //Route Handler 2
+      console.log("Handling the router user 2");
+      next();
+    },
+  ],
+  (req, res, next) => {
+    //Route Handler 3
+    console.log("Handling the router user 3");
+    next();
+  },
+  (req, res, next) => {
+    //Route Handler 4
+    console.log("Handling the router user 4");
+    next();
+  },
+  (req, res, next) => {
+    //Route Handler 5
+    console.log("Handling the router user 5");
+    res.send("Route Handler 5");
+  }
+);
+
+app.listen(3000, () => {
+  console.log("Server is successfully listening on port 3000...");
+});
