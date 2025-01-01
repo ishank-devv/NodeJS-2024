0963894e92ee8c50ba90362d90724732dde6ef20
Ishank Suri <ishanksuri01@gmail.com>
2025-01-01 20:08:11 +0530

    Error Handling

--- S02/E05-middleware-route-handlers-error-handlers/devTinder-backend/src/Error_handling1.js
+++ S02/E05-middleware-route-handlers-error-handlers/devTinder-backend/src/Error_handling1.js
@@ -1,23 +1,21 @@
-// ERROR HANDLING 2
-
 const express = require("express");
 const app = express();
 
 app.get("/getUserData", (req, res) => {
-  try {
-    // Logic of DB call and get user data
-    throw new Error("dvbzhjf");
-  } catch (err) {
-    res.status(500).send("Something went wrong");
-  }
+  //testing error handling
+  throw new Error("not a random errorr");
+
+  // Logic of DB call and get user data
+  res.send("User Data sent");
 });
 
 //ERROR HANDLING here ( if you don't use try catch block above- if you didnt use try catch)
 // err should be the first parameter- order matters a lot
-// app.use("/", (err, req, res, next) => {
-//   if (err) {
-//   }
-// });
+app.use("/", (err, req, res, next) => {
+  if (err) {
+    res.status(500).send("Something went wrong");
+  }
+});
 
 app.listen(3000, () => {
   console.log("Server is successfully listening on port 3000...");
