8d5b65415bf8a80ef00a981aeb0529b275baa489
Ishank Suri <ishanksuri01@gmail.com>
2025-01-03 22:38:58 +0530

    Intialising a DB & saving data in it

--- S02/E06-Database-schema-models-mongoose/devTinder-backend/src/app.js
+++ S02/E06-Database-schema-models-mongoose/devTinder-backend/src/app.js
@@ -1,6 +1,27 @@
 const express = require("express");
 const connectDB = require("./config/database");
 const app = express();
+const User = require("./models/user");
+
+app.post("/signup", async (req, res) => {
+  // to save this userObj into user collection,
+  // First, we'll have to create new instance of User model & pass the data
+  //  ( creating a new user with above data )
+  const user = new User({
+    firstName: "Sachin",
+    lastName: "Tendulkar",
+    emailId: "sachin@gupta.com",
+    password: "sachin@123",
+  });
+  // NOTE: always do DB operation in try catch block
+  try {
+    // this will save data on DB, this funct returns you a promise( most mongoose functions does)
+    await user.save();
+    res.send("User Added successfully!");
+  } catch (err) {
+    res.status(400).send("Error saving the user:" + err.message);
+  }
+});
 
 connectDB()
   .then(() => {
